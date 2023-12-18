abdd72c2032cfa900fa6d0c353b77a39782a2e37
jp02234 <jp6909236@gmail.com>
2023-12-18 08:02:00 +0530

    7-mvc

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,17 @@
-const products = [];
+const products  = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.status(200).render('add-product', { pageTitle: 'Add Product',  path: 'admin/add-product'});
 };
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new products(req.body.title)
+    product.save()
     res.redirect('/');
 };
 
 exports.getProduct = (req, res, next) => {
-    res.render('shop', { prods: products, pageTitle: 'Shop', path: '/'});
+    products.fetchAll( products => {
+        res.render('shop', { prods: products, pageTitle: 'Shop', path: '/'});
+    });
 };
